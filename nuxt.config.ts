da548e2f7da9a864c9ccf82b43a9a3a9d32cd4ec
Faza Ghani Irham <fghaniirham1@gmail.com>
2025-12-16 16:03:41 +0700

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
+  fonts: {
+    families: [
+      {
+        name: 'Roboto',
+      },
+      {
+        name: 'Zalando Sans Expanded',
+      }
+    ]
+  },
+  modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/image', '@nuxt/eslint'],
+})
