0a8d4bfb388311b6689ae0a593d3658cdc7a99cc
Faza Ghani Irham <fghaniirham1@gmail.com>
2026-04-06 11:07:20 +0700

    feat: playground app button

---
+++ app/pages/playground/app-button/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const openInstagram = () => {
+  window.open('https://instagram.com')
+}
+</script>
+
+<template>
+  <PageContainer>
+    <UButton type="button" @click="openInstagram">Open Instagram</UButton>
+  </PageContainer>
+</template>
