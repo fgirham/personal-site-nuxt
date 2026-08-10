6344d86996806074143c50d2f3e18880bba4c630
Faza Ghani Irham <fghaniirham1@gmail.com>
2026-08-10 16:09:01 +0700

    feat: try camera frame

---
+++ app/pages/playground/camera-frame/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const videoElement = ref()
+
+const startCamera = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoElement.value.srcObject = stream;
+  } catch (error) {
+    console.error('Camera access denied or not supported:', error);
+  }
+}
+</script>
+
+<template>
+  <PageContainer>
+    <video id="webcam" ref="videoElement" autoplay playsinline width="640" height="480"></video>
+    <UButton @click="startCamera">Start Camera</UButton>
+  </PageContainer>
+</template>
